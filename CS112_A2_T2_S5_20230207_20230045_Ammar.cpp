1a0f01a7b328c213bda1a3ce5418a96753ebed56
Adham Ghallab <adhamghallab0@gmail.com>
2024-03-08 17:37:38 +0200

    Created The C++ File, we'll be working on

---
+++ CS112_A2_T2_S5_20230207_20230045_Ammar.cpp
@@ -0,0 +1,7 @@
+// File: CS112_A2_T2_S5_20230207_20230045_Ammar.............cpp
+// Purpose: 3 Cipher Algorithms corresponding to the latest digit in our IDs 
+// Author: Adham Mahmoud Mohamed Ghallab, Ammar ......... Sayed, Abdelrahman Khaled Mohamed Sonosy
+// Who did which cipher: ......................................................
+// Emails: ...............................
+// IDs: Your IDs and who did which part................ 20230045 --> Substitution Cipher
+
